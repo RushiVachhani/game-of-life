11c792ee40bd57371d1b95483591b8dde6f87d79
RushiVachhani <vachhanirushi30@gmail.com>
2021-10-16 19:13:42 +0530

    refactor code

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,19 +2,32 @@ import { useEffect, useState } from "react";
 
 import Grid from "./Grid";
 import useInterval from "../hooks/useInterval";
+import { constants } from "../utils/constants";
 
 const Game = () => {
-  const rows = 30;
-  const cols = 50;
+  // states
   const [generations, setGenerations] = useState(0);
-  const [running, setRunning] = useState(false);
-  const [speed, setSpeed] = useState(500);
+  const [running, setRunning] = useState(constants.initialRunning);
+  const [speed, setSpeed] = useState(constants.initialSpeed);
+  const [rows, setRows] = useState(constants.initialRows);
+  const [cols, setCols] = useState(constants.initialCols);
   const [fullGrid, setFullGrid] = useState(
     Array(rows)
       .fill()
       .map(() => Array(cols).fill(false))
   );
 
+  // lifecycle methods
+  useEffect(() => {
+    seed();
+  }, []);
+
+  // hooks
+  useInterval(() => {
+    play();
+  }, speed);
+
+  // functions
   const selectBox = (row, col) => {
     let fullGridClone = JSON.parse(JSON.stringify(fullGrid));
     fullGridClone[row][col] = !fullGrid[row][col];
@@ -29,7 +42,9 @@ const Game = () => {
       .map(() => Array(cols).fill(false));
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
-        if (Math.floor(Math.random() * 10) <= 3) {
+        if (
+          Math.floor(Math.random() * 10) <= constants.RandomizationParameter
+        ) {
           fullGridClone[i][j] = true;
         }
       }
@@ -38,28 +53,53 @@ const Game = () => {
   };
 
   const handlePlay = () => {
-    setRunning(true);
+    if (running) {
+      setGenerations(0);
+    }
+    setRunning((previous) => !previous);
+    seed();
   };
-
-  useInterval(() => {
-    play();
-  }, speed);
 
   const handlePause = () => {
     setRunning(false);
   };
 
   const handleSlow = () => {
-    setSpeed(1000);
+    setSpeed(constants.slowSpeed);
   };
 
   const handleFast = () => {
-    setSpeed(100);
+    setSpeed(constants.fastSpeed);
   };
 
   const handleSeed = () => {
+    setRunning(false);
+    setGenerations(0);
     seed();
   };
+
+  const handleClear = () => {
+    setRunning(false);
+    setFullGrid(
+      Array(rows)
+        .fill()
+        .map(() => Array(cols).fill(false))
+    );
+    setGenerations(0);
+  };
+
+  // const handleGridSizeChange = async (e) => {
+  //   const newRows = e.target.value.split("-")[0];
+  //   const newCols = e.target.value.split("-")[1];
+  //   // await setFullGrid(
+  //   //   Array(newRows)
+  //   //     .fill()
+  //   //     .map(() => Array(newCols).fill(false))
+  //   // );
+  //   // setRows(newRows);
+  //   // setCols(newCols);
+  //   // seed();
+  // };
 
   const play = () => {
     if (!running) {
@@ -87,15 +127,11 @@ const Game = () => {
     setGenerations((prev) => prev + 1);
   };
 
-  useEffect(() => {
-    seed();
-  }, []);
-
   return (
     <main className="container">
       <div class="button-section">
         <button type="button" className="btn" onClick={handlePlay}>
-          Play
+          {running ? "Stop" : "Play"}
         </button>
         <button type="button" className="btn" onClick={handlePause}>
           Pause
@@ -109,6 +145,19 @@ const Game = () => {
         <button type="button" className="btn" onClick={handleSeed}>
           Seed
         </button>
+        <button type="button" className="btn" onClick={handleClear}>
+          Clear
+        </button>
+        {/* <select onChange={handleGridSizeChange}>
+          <option value="30-30">30 X 30</option>
+          <option value="30-50" selected>
+            30 X 50
+          </option>
+          <option value="30-70">30 X 70</option>
+          <option value="50-30">50 X 30</option>
+          <option value="50-50">50 X 50</option>
+          <option value="50-70">50 X 70</option>
+        </select> */}
       </div>
       <Grid fullGrid={fullGrid} rows={rows} cols={cols} selectBox={selectBox} />
       <h2>Generations: {generations}</h2>
