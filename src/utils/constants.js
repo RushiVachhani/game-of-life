6e8d5808210346c72e47cabda9caa18c16754308
RushiVachhani <vachhanirushi30@gmail.com>
2021-10-16 19:13:23 +0530

    add constants for config

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const constants = {
+  initialRows: 30,
+  initialCols: 50,
+  initialSpeed: 500,
+  RandomizationParameter: 4,
+  slowSpeed: 1000,
+  fastSpeed: 100,
+  initialRunning: false,
+};
